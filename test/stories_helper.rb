7139ca1ab7be06ad974727bfa7b50aca29248068
Matías Flores <mflores@atlanware.com>
2010-09-13 14:20:42 -0300

    Use rack_test as the default Capybara driver

--- test/stories_helper.rb
+++ test/stories_helper.rb
@@ -7,13 +7,13 @@ require "capybara"
 require "capybara/dsl"
 require "protest/stories"
 
-Capybara.default_driver = :selenium
+Capybara.default_driver = :rack_test
 Capybara.app = Main
 
 class Protest::TestCase
   include Capybara
 
-  def assert_contain text
+  def assert_contain(text)
     assert page.has_content?(text)
   end
 
@@ -21,7 +21,7 @@ class Protest::TestCase
     Capybara.current_session.driver.rack_server.response.status
   end
 
-  def url path
+  def url(path)
     Capybara.current_session.driver.rack_server.url path
   end
 end
